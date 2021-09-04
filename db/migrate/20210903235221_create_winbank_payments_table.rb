795afba3f033c3617b59f930abac79ed8ee88398
dimidev <dimidev@olympusone.com>
2021-09-04 18:12:03 +0300

    up

--- db/migrate/20210903235221_create_winbank_payments_table.rb
+++ db/migrate/20210903235221_create_winbank_payments_table.rb
@@ -1,7 +1,7 @@
 class CreateWinbankPaymentsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :spree_winbank_payments do |t|
-      t.references :payments
+      t.references :payment
       t.string :transaction_ticket, index:{unique: true}, null: false
       t.timestamps
     end
