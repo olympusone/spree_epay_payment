4a7e0bd1872ef7bd08b72a8fd82824a15f45b361
dimidev <dimidev@olympusone.com>
2021-09-04 03:12:21 +0300

    up

--- app/models/spree/winbank_payment.rb
+++ app/models/spree/winbank_payment.rb
@@ -1,6 +1,6 @@
 module Spree
     class WinbankPayment < Spree::Base
-        validates :transaction_ticket, presence :true, uniqueness: {case_sensitive: false}
+        validates :transaction_ticket, presence: true, uniqueness: {case_sensitive: false}
     
         belongs_to :payment
     end
