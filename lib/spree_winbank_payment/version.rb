2bfa4064e47c85999486af9b51399eec44f2f997
dimidev <dimidev@olympusone.com>
2022-06-04 22:00:42 +0300

    fix

--- lib/spree_winbank_payment/version.rb
+++ lib/spree_winbank_payment/version.rb
@@ -1,9 +1,9 @@
-module SpreeEurobankPayment
+module SpreeWinbankPayment
   VERSION = '1.0.0'.freeze
 
   module_function
 
-  # Returns the version of the currently loaded SpreeEurobankPayment as a
+  # Returns the version of the currently loaded SpreeWinbankPayment as a
   # <tt>Gem::Version</tt>.
   def version
     Gem::Version.new VERSION
